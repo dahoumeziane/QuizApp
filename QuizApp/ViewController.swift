65111c9f5b54945e35edc2f196bd62d891044024
Meziane Dahou <hd_meziane@esi.dz>
2020-12-23 18:25:56 +0100

    first commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Dahou Meziane on 12/23/20.
+//  Copyright © 2020 Meziane Dahou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
